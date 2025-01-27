0718b3c03526b5ce10271bf8ca45c10c761aaf1a
nocodecampo <dkampo@gmail.com>
2025-01-27 16:49:38 +0100

    página login

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+    <section class="section-login">
+        <!--Formulario de login con username y password-->
+        <form action="" method="post" class="login-form">
+            <label for="username">Usuario</label>
+            <input type="text" name="username" id="username" placeholder="Tu usuario" required>
+            <label for="password">Contraseña</label>
+            <input type="password" name="password" id="password" placeholder="Tu contraseña" required>
+            <input type="submit" value="Iniciar sesión">
+        </form>
+    </section>
+</body>
+</html>

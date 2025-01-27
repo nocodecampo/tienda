1e975c8ef7e28e52037e421839d564ee18c912c7
nocodecampo <dkampo@gmail.com>
2025-01-27 17:00:37 +0100

    dbconnect

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 <link rel="stylesheet" href="style.css">
 <body>
     <section class="section-login">
+        <div class="form-container">
+            <h1>Bienvenido a la Tienda</h1>
         <!--Formulario de login con username y password-->
         <form action="" method="post" class="login-form">
             <label for="username">Usuario</label>
@@ -16,6 +18,7 @@
             <input type="password" name="password" id="password" placeholder="Tu contraseña" required>
             <input type="submit" value="Iniciar sesión">
         </form>
+        </div>
     </section>
 </body>
 </html>

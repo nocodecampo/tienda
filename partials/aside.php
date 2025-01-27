fbbc8cc089fd9559785ba65a82648d1241e861ad
nocodecampo <dkampo@gmail.com>
2025-01-27 20:25:00 +0100

    menu aside

--- partials/aside.php
+++ partials/aside.php
@@ -1,12 +1,12 @@
 <aside class="admin-menu">
-            <h2>Menú</h2>
-            <nav>
-                <ul>
-                    <li><a href="#">Proveedores</a></li>
-                    <li><a href="#">Clientes</a></li>
-                    <li><a href="#">Categorías</a></li>
-                    <li><a href="#">Productos</a></li>
-                    <li><a href="#">Ventas</a></li>
-                </ul>
-            </nav>
-        </aside>
+    <nav>
+        <ul>
+            <li><a href="admin"><i class="fa-solid fa-house"></i>Inicio</a></li>
+            <li><a href="#"><i class="fa-solid fa-truck-field"></i>Proveedores</a></li>
+            <li><a href="#"><i class="fa-solid fa-user"></i>Clientes</a></li>
+            <li><a href="#"><i class="fa-solid fa-list"></i>Categorías</a></li>
+            <li><a href="#"><i class="fa-solid fa-box"></i>Productos</a></li>
+            <li><a href="#"><i class="fa-solid fa-money-bill"></i>Ventas</a></li>
+        </ul>
+    </nav>
+</aside>
--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,8 @@
 <header class="header-admin">
-        <img src="img/logo.png" alt="LOGO" class="logo">
-        <div>
-            <p class="welcome">Bienvenido, <?php echo $user['username']; ?></p>
-            <a href="logout.php" class="logout-button">Cerrar sesión</a>
-        </div>
-    </header>
+    <i class="fa-solid fa-store"></i>
+    <!--<img src="img/logo.png" alt="LOGO" class="logo">-->
+    <div>
+        <p class="welcome">Bienvenido, <?php echo $user['username']; ?></p>
+        <a href="logout.php" class="logout-button">Cerrar sesión</a>
+    </div>
+</header>

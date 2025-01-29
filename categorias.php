eefbd180ceb7d76d9acb6bca96ca1d9bfd3a746a
nocodecampo <dkampo@gmail.com>
2025-01-29 19:25:01 +0100

    añadir categorías

--- categorias.php
+++ categorias.php
@@ -1,8 +1,8 @@
 <!-- incluimos la cabecera desde el archivo header.php -->
 <?php
 include 'partials/header.php';
-// Consulta a la base de datos para obtener los proveedores
-$sql = "SELECT * FROM categorias order by id desc";
+// Consulta a la base de datos para obtener las categorías
+$sql = "SELECT * FROM categorias order by id asc";
 $stmt = $conn->prepare($sql);
 ?>
 <section class="admin-panel">
@@ -10,6 +10,22 @@ $stmt = $conn->prepare($sql);
     <?php include 'partials/aside.php' ?>
     <main class="admin-content">
         <h1>Categorías</h1>
+        <hr>
+        <div class="add-form-container">
+            <h3>Añadir categoría</h3>
+            <form action="nueva_categoria.php" method="post" class="nueva-categoria">
+                <div>
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" name="nombre" id="nombre" placeholder="Nombre categoría" required>
+                </div>
+                <div>
+                    <label for="descripcion">Descripción:</label>
+                    <input type="text" name="descripcion" id="descripcion" placeholder="Añade descripción" required>
+                </div>
+                <input type="submit" value="Añadir">
+            </form>
+        </div>
+        <hr>
         <table class="tabla-resultados">
             <thead>
                 <tr>
@@ -35,20 +51,6 @@ $stmt = $conn->prepare($sql);
                 <?php } ?>
             </tbody>
         </table>
-        <div class="add-form-container">
-            <h3>Añadir categoría</h3>
-            <form action="nueva_categoria.php" method="post" class="nuevo-proveedor">
-                <div>
-                    <label for="nombre">Nombre:</label>
-                    <input type="text" name="nombre" id="nombre" placeholder="Nombre categoría" required>
-                </div>
-                <div>
-                    <label for="descripcion">Descripción:</label>
-                    <input type="text" name="descripcion" id="descripcion" placeholder="Añade descripción" required>
-                </div>
-                <input type="submit" value="Añadir">
-            </form>
-        </div>
     </main>
 </section>
 <!-- incluimos el footer desde el archivo footer.php -->
